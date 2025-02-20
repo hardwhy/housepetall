7f29a871b48cfa520c6c2b7d32317a4413180c26
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-02-20 07:26:02 +0700

    refine: separate review content to individual file for reusability

--- lib/src/presentation/screen/review/widgets/review_card.dart
+++ lib/src/presentation/screen/review/widgets/review_card.dart
@@ -1,9 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 import 'package:housepetall/src/domain/domain.dart';
+import 'package:housepetall/src/presentation/screen/review/review.dart';
+import 'package:housepetall/src/presentation/screen/review/review_detail.dart';
+import 'package:housepetall/src/presentation/screen/review/widgets/review_content.dart';
 import 'package:housepetall/src/presentation/themes/themes.dart';
 import 'package:housepetall/src/presentation/widgets/widgets.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -60,135 +61,18 @@ class ReviewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Ripplify(
-      onTap: () {},
+      onTap: () {
+        final args = ReviewDetailProp(review: review);
+        Navigator.pushNamed(context, ReviewRoutes.detail, arguments: args);
+      },
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
         boxShadow: Shadows.elevation2(color: Colors.black38),
       ),
       constraints: BoxConstraints(maxHeight: 200, minHeight: 150),
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          HeaderSection(review: review),
-          if (review.comment != null)
-            Flexible(child: CommentSection(review: review))
-        ],
-      ),
-    );
-  }
-}
-
-class CommentSection extends StatelessWidget {
-  const CommentSection({
-    super.key,
-    required this.review,
-  });
-
-  final Review review;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Divider(color: Colors.deepOrange[50]),
-        Flexible(
-          child: Paragraph(
-            review.comment!,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        )
-      ],
-    );
-  }
-}
-
-class HeaderSection extends StatelessWidget {
-  const HeaderSection({
-    super.key,
-    required this.review,
-  });
-
-  final Review review;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [_buildOwnerRow(), SizedBox(height: 8), _buildPetRow()],
-        ),
-        Rating(rating: review.rating),
-      ],
-    );
-  }
-
-  Widget _buildOwnerRow() {
-    return Row(
-      children: [
-        _buildAvatar(review.ownerName[0]),
-        const SizedBox(width: 8),
-        Body1.bold(review.ownerName),
-      ],
-    );
-  }
-
-  Widget _buildPetRow() {
-    return Row(
-      children: [
-        _buildAvatar(review.petName[0]),
-        const SizedBox(width: 8),
-        Body1(review.petName),
-      ],
-    );
-  }
-
-  Widget _buildAvatar(String initial) {
-    return CircleAvatar(
-      radius: 15,
-      backgroundColor: Colors.deepOrange[((Random().nextInt(9) + 1) * 100)],
-      child: Body1.bold(initial, color: Colors.white),
-    );
-  }
-}
-
-class Rating extends StatelessWidget {
-  const Rating({
-    super.key,
-    required this.rating,
-  });
-
-  final int rating;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: ListView.separated(
-        shrinkWrap: true,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          final order = index + 1;
-          Color? color = Colors.grey[300];
-          if (order <= rating) {
-            color = Colors.amber[600];
-          }
-          return Icon(
-            Icons.star_rounded,
-            color: color,
-            size: 24,
-          );
-        },
-        separatorBuilder: (context, index) => SizedBox(width: 2),
-        itemCount: 4,
-      ),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: ReviewContent(prop: ReviewContentProp(review: review)),
     );
   }
 }
---
+++ lib/src/presentation/screen/review/widgets/review_content.dart
@@ -0,0 +1,178 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'package:housepetall/src/domain/domain.dart';
+import 'package:housepetall/src/presentation/screen/review/review_constants.dart';
+import 'package:housepetall/src/presentation/themes/themes.dart';
+import 'package:sprintf/sprintf.dart';
+
+class ReviewContentProp {
+  final Review review;
+  final bool compactMode;
+
+  ReviewContentProp({
+    required this.review,
+    this.compactMode = true,
+  });
+}
+
+class ReviewContent extends StatelessWidget {
+  final ReviewContentProp prop;
+  const ReviewContent({
+    super.key,
+    required this.prop,
+  });
+
+  Review get _review => prop.review;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        HeaderSection(prop: prop),
+        if (_review.comment != null) Flexible(child: CommentSection(prop: prop))
+      ],
+    );
+  }
+}
+
+class CommentSection extends StatelessWidget {
+  const CommentSection({
+    super.key,
+    required this.prop,
+  });
+
+  final ReviewContentProp prop;
+
+  String get _comment => prop.review.comment ?? '';
+  bool get _compactMode => prop.compactMode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const SizedBox(height: 8),
+        const Divider(),
+        const SizedBox(height: 8),
+        Flexible(
+          child: Paragraph(
+            _comment,
+            maxLines: _compactMode ? 2 : null,
+            overflow: _compactMode ? TextOverflow.ellipsis : null,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({
+    super.key,
+    required this.prop,
+  });
+
+  final ReviewContentProp prop;
+
+  Review get _review => prop.review;
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: sprintf(
+        ReviewConstants.heroTags.reviewContent,
+        [_review.id],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (prop.compactMode) ...[
+                _buildOwnerRow(),
+                SizedBox(height: 8),
+              ],
+              _buildPetRow(),
+            ],
+          ),
+          Rating(rating: _review.rating),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildOwnerRow() {
+    return Flexible(
+      child: Row(
+        children: [
+          _buildAvatar(_review.ownerName[0]),
+          const SizedBox(width: 8),
+          Body1.bold(_review.ownerName),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPetRow() {
+    return Flexible(
+      child: Row(
+        children: [
+          _buildAvatar(_review.petName[0]),
+          const SizedBox(width: 8),
+          Body1(_review.petName),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAvatar(String initial) {
+    return CircleAvatar(
+      radius: 15,
+      backgroundColor: Colors.deepOrange[((Random().nextInt(9) + 1) * 100)],
+      child: Body1.bold(initial, color: Colors.white),
+    );
+  }
+}
+
+class Rating extends StatelessWidget {
+  const Rating({
+    super.key,
+    required this.rating,
+  });
+
+  final int rating;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: ListView.separated(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final order = index + 1;
+          Color? color = Colors.grey[300];
+          if (order <= rating) {
+            color = Colors.amber[600];
+          }
+          return Icon(
+            Icons.star_rounded,
+            color: color,
+            size: 24,
+          );
+        },
+        separatorBuilder: (context, index) => SizedBox(width: 2),
+        itemCount: 4,
+      ),
+    );
+  }
+}
