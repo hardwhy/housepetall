c28e03253a0066d4bf7edffbef9b2cde403e5aef
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-02-20 07:24:36 +0700

    refine: add divider theme

--- lib/src/presentation/themes/src/theme_data.dart
+++ lib/src/presentation/themes/src/theme_data.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class HousePetAllThemes {
-  static final basic = ThemeData(scaffoldBackgroundColor: Colors.white);
-  static final darkBasic = ThemeData.dark(useMaterial3: true);
+  static final basic = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    dividerColor: Colors.deepOrange[50],
+    dividerTheme: DividerThemeData(color: Colors.deepOrange[50], thickness: 2),
+  );
+  static final darkBasic = ThemeData.dark(
+    useMaterial3: true,
+  );
 }
