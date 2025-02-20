010f914e978c03e9ed155306bf443a449639f912
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-02-20 17:17:16 +0700

    refactor: fix endpoint since host already specified in dio option

--- lib/src/common/constants/endpoints.dart
+++ lib/src/common/constants/endpoints.dart
@@ -1,11 +1,9 @@
-import 'package:housepetall/config.dart';
-
 class Endpoints {
   static const review = _Review();
 }
 
 class _Review {
   const _Review();
-  String get getReviews => '${Config.host}/reviews/';
-  String get createReviews => '${Config.host}/reviews/create';
+  String get getReviews => '/reviews/';
+  String get createReviews => '/reviews/create';
 }
