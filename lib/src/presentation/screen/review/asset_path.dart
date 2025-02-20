862bb201f85a5aa31ed3ea8e13f999720bd74502
hardwhy <ayi.hardiyanto@dkatalis.com>
2025-02-20 07:23:38 +0700

    chores: add detail asset

--- lib/src/presentation/screen/review/asset_path.dart
+++
@@ -1,4 +0,0 @@
-class ReviewAssetPath {
-  static const root = 'assets/images/reviews';
-  static const empty = '$root/empty.svg';
-}
---
+++ lib/src/presentation/screen/review/review_constants.dart
@@ -0,0 +1,18 @@
+class ReviewConstants {
+  static const assets = _ReviewAssetPath();
+  static const heroTags = _ReviewHeroTags();
+}
+
+class _ReviewAssetPath {
+  const _ReviewAssetPath();
+  String get root => 'assets/images/reviews';
+  String get empty => '$root/empty.svg';
+  String get detail => '$root/detail.svg';
+}
+
+class _ReviewHeroTags {
+  const _ReviewHeroTags();
+
+  String get reviewContent => 'reviewContent%s';
+}
+
